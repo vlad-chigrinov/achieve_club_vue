62c973bd16a383e827b5ee1ff6f929f6cc06f1fa
FatAlien <vladislav.chigrinov@gmail.com>
2024-11-18 17:05:40 +0300

    Revert "Register info and anotherUser pages on router"
    This reverts commit 3f501dbd0732b15959cbf0ee2d4a5ede5a656a7f.

--- src/main.js
+++ src/main.js
@@ -9,14 +9,12 @@ import HomePage from './Pages/HomePage.vue'
 import LoginPage from './Pages/LoginPage.vue'
 import RegistrationPage from './Pages/RegistrationPage.vue'
 import TopUsersPage from './Pages/TopUsersPage.vue'
-import AnotherUserPage from './Pages/AnotherUserPage.vue'
 
 const routes = [
   { path: '/', component: HomePage },
   { path: '/login', component: LoginPage },
   { path: '/registration', component: RegistrationPage },
   { path: '/topusers', component: TopUsersPage },
-  { path: '/users/:id', component: AnotherUserPage }
 ]
 
 const pinia = createPinia()
