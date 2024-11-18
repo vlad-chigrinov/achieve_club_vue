07cb6c8755204620ef957b3be3790060c10ae8e4
FatAlien <vladislav.chigrinov@gmail.com>
2024-11-18 17:06:09 +0300

    #2 Register info and anotherUser pages on router

--- src/main.js
+++ src/main.js
@@ -9,12 +9,16 @@ import HomePage from './Pages/HomePage.vue'
 import LoginPage from './Pages/LoginPage.vue'
 import RegistrationPage from './Pages/RegistrationPage.vue'
 import TopUsersPage from './Pages/TopUsersPage.vue'
+import InfoPage from './Pages/InfoPage.vue'
+import AnotherUserPage from './Pages/AnotherUserPage.vue'
 
 const routes = [
   { path: '/', component: HomePage },
   { path: '/login', component: LoginPage },
   { path: '/registration', component: RegistrationPage },
   { path: '/topusers', component: TopUsersPage },
+  { path: '/info', component: InfoPage },
+  { path: '/users/:id', component: AnotherUserPage }
 ]
 
 const pinia = createPinia()
