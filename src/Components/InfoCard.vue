4417f178d0cfbfb11a28837db14d79045b2c6212
FatAlien <vladislav.chigrinov@gmail.com>
2024-11-20 14:39:17 +0300

    Impove infopage

--- src/Components/InfoCard.vue
+++ src/Components/InfoCard.vue
@@ -1,5 +1,40 @@
 <template>
-  <slot name="icon" />
-  <slot name="title" />
-  <slot name="content" />
+  <div class="slide">
+    <div class="icon">
+      <slot name="icon" />
+    </div>
+    <h3 class="title">
+      <slot name="title" />
+    </h3>
+    <div class="content">
+      <slot name="content" />
+    </div>
+  </div>
 </template>
+<style scoped>
+.slide {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 10px;
+  margin: 10px 20px 10px 20px;
+}
+
+.icon {
+  font-size: 40pt;
+  line-height: 70pt;
+}
+
+.title {
+  font-size: 18pt;
+}
+
+.content {
+  text-align: left;
+  display: flex;
+  flex-direction: column;
+  gap: 5px;
+  list-style-position: inside;
+  max-width: 500px;
+}
+</style>
--- src/Pages/InfoPage.vue
+++ src/Pages/InfoPage.vue
@@ -5,75 +5,98 @@ import InfoCard from '@/Components/InfoCard.vue'
 import { Swiper, SwiperSlide } from 'swiper/vue'
 import { Navigation, Pagination } from 'swiper/modules'
 import 'swiper/css'
-import 'swiper/css/navigation'
 import 'swiper/css/pagination'
+import 'swiper/css/navigation'
 </script>
 <template>
   <main-layout :tab="Number(3)">
-    <h1 class="page-title">О приложении</h1>
     <div class="content-wrapper">
       <swiper
         class="corousel"
         :navigation="true"
         :pagination="true"
-        :modules="[Navigation, Pagination]"
+        :modules="[Pagination, Navigation]"
       >
-        <swiper-slide
-          ><info-card>
+        <swiper-slide>
+          <info-card>
             <template #icon>
-              <i class="fa-solid fa-qrcode"></i>
+              <i class="fa-solid fa-qrcode fa-2xl"></i>
             </template>
             <template #title> Выполнение достижений </template>
             <template #content>
-              Чтобы выполнить достижение вам нужно: Выбрать желаемое достижение Нажать на вспывающую
-              кнопку
+              <p>Чтобы выполнить достижение вам нужно:</p>
+              <ul>
+                <li>Выбрать желаемое достижение</li>
+                <li>Нажать на вспывающую кнопку</li>
+                <li>Попросить у тренера просканировать код</li>
+              </ul>
             </template>
-          </info-card></swiper-slide
-        >
-        <swiper-slide>Slide 2</swiper-slide>
-        <swiper-slide>Slide 3</swiper-slide>
+          </info-card>
+        </swiper-slide>
+        <swiper-slide>
+          <info-card>
+            <template #icon>
+              <i class="fa-regular fa-star-christmas fa-2xl"></i>
+            </template>
+            <template #title> Получайте XP за достижения </template>
+            <template #content>
+              <p>Чем больше достижений</p>
+              <i class="fa-solid fa-arrow-down" style="text-align: center"></i>
+              <p>Тем больше XP</p>
+              <i class="fa-solid fa-arrow-down" style="text-align: center"></i>
+              <p>Тем выше вы в топе</p>
+            </template>
+          </info-card>
+        </swiper-slide>
+        <swiper-slide>
+          <info-card>
+            <template #icon>
+              <i class="fa-solid fa-sparkles fa-2xl"></i>
+            </template>
+            <template #title> Комбо-достижения </template>
+            <template #content>
+              <p>
+                Все комбо достижения отображаются во вкладке <b>КОМБО</b>. Их вы можете выполнять
+                СКОЛЬКО УГОДКО - и каждый раз получать за это очки.
+              </p>
+              <p>
+                <i>
+                  К примеру: если вы выполнили такое достижение 10 раз, то получили в 10 раз больше
+                  очков!
+                </i>
+              </p>
+            </template>
+          </info-card>
+        </swiper-slide>
       </swiper>
     </div>
   </main-layout>
 </template>
 <style scoped>
-.page-title {
-  color: var(--primary);
-  font-size: 18pt;
-  text-align: center;
-  line-height: 40px;
-}
-
 .content-wrapper {
-  width: 100%;
-  height: 100dvh;
   display: flex;
-  align-items: center;
+  flex-direction: column;
   justify-content: center;
+  align-items: center;
+  height: 100dvh;
+  padding-bottom: 100px;
 }
 
 .swiper {
+  width: 100%;
   height: 100%;
-  margin: 10px 20px 200px 20px;
   --swiper-theme-color: var(--primary);
-  --swiper-navigation-size: 20px;
+  --swiper-navigation-size: 20pt;
+  --swiper-navigation-sides-offset: 20%;
+  --swiper-navigation-top-offset: 30%;
 }
 
 .swiper-slide {
   color: var(--on-tertiary);
   text-align: center;
   font-size: 18px;
-
-  /* Center slide text vertically */
   display: flex;
+  flex-direction: column;
   justify-content: center;
-  align-items: center;
-}
-
-.swiper-slide img {
-  display: block;
-  width: 100%;
-  height: 100%;
-  object-fit: cover;
 }
 </style>
